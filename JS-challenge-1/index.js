f68cd88c8603a371a99e0983cc0d230af2a75cf9
Brayan Cataño <brayansanchez67@hotmail.com>
2024-03-12 17:02:23 -0500

    fist commit

---
+++ JS-challenge-1/index.js
@@ -0,0 +1,42 @@
+class Cinema {
+   rows;
+   seats;
+
+   status() {
+      
+   }
+   
+   constructor(rows, seats){
+
+      this.rows = [];
+      for(let i = 0; i < rows: i++){
+         //TODO
+      }
+   }
+}
+
+class Seat {
+   rowNum;
+   rowSeat;
+   status;
+
+   constructor(rowNum, rowSeat, status){
+      this.rowNum;
+      this.rowSeat;
+      this.status;
+   }
+}
+
+class Row{
+   seats;
+
+   constructor(seats){
+      this.seats = seats;
+   }
+}
+
+
+const cinema = new Cinema(5,10)
+
+cinema.status();
+
