de77837629b5e1b48da1467d7063b1d37705a44a
Brayan Cataño <brayansanchez67@hotmail.com>
2024-03-18 13:09:23 -0500

    added find non repeating element in array

--- JS-challenge-3/index.js
+++ JS-challenge-3/index.js
@@ -1,3 +1,11 @@
 let arr2 = [7,9,1,'a','a','f',9,4,2,'d','d']
 
 console.log(arr2.filter((item, index) => arr2.indexOf(item) === index))
+
+
+let arr = [1,2,4,7,4,2,1]
+
+console.log(arr.reduce((acc, curr) => acc ^ curr, 0))
+
+
+// console.log(1 ^ 2 ^ 4 ^ 7 ^ 4 ^ 2 ^ 1)
