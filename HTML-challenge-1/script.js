f5b661bae9a1986e92d775b273adc025486229b7
Brayan Cataño <brayansanchez67@hotmail.com>
2024-03-21 17:11:55 -0500

    added HTML chall 1

---
+++ HTML-challenge-1/script.js
@@ -0,0 +1,11 @@
+class Card extends HTMLElement {
+   constructor(){
+      super()
+   }
+}
+
+customElements.define('jala-card', Card);  
+
+// document.getElementById('jala-card').attachShadow({ mode: "open" })
+
+
