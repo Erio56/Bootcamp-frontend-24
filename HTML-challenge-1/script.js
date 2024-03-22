13e666a6778a201cbbd9e2985d8586534c8c2bea
Brayan Cataño <brayansanchez67@hotmail.com>
2024-03-22 17:08:21 -0500

    fixed component

--- HTML-challenge-1/script.js
+++ HTML-challenge-1/script.js
@@ -1,11 +1,16 @@
 class Card extends HTMLElement {
    constructor(){
       super()
+      const shadowRoot = this.attachShadow({ mode: 'open' })
+      const template = document.getElementById("card-template");
+      const templateContent = template.content.cloneNode(true);
+      shadowRoot.appendChild(templateContent)
    }
 }
 
 customElements.define('jala-card', Card);  
 
-// document.getElementById('jala-card').attachShadow({ mode: "open" })
+
+
 
 
