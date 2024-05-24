220a000e9b69f572271782297254ae3582db5ea4
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-24 14:51:18 -0500

    fixed navbar logo styles

--- final-proyect/src/components/atoms/NavbarLogo.tsx
+++ final-proyect/src/components/atoms/NavbarLogo.tsx
@@ -1,9 +1,11 @@
+import NavbarItem from "./NavbarItem";
 
 const NavbarLogo = () => {
 
    return (
-      <div>
+      <div className="navbar_logo">
          <img src="assets/pokemon_logo.svg" alt="poke_logo" height={60} />
+         <NavbarItem label="Frontend Final Project"/>
       </div>
    )
 }
