51bd91b1e3d9ab0bf302e678f5feaded44506d45
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-24 14:06:44 -0500

    added logo

---
+++ final-proyect/src/components/atoms/NavbarLogo.tsx
@@ -0,0 +1,12 @@
+
+const NavbarLogo = () => {
+
+   return (
+      <div>
+         <img src="assets/pokemon_logo.svg" alt="poke_logo" height={60} />
+      </div>
+   )
+}
+
+
+export default NavbarLogo;
--- final-proyect/src/components/molecules/NavbarElements.tsx
+++ final-proyect/src/components/molecules/NavbarElements.tsx
@@ -1,9 +1,12 @@
 import NavbarItem from "../atoms/NavbarItem"
+import NavbarLogo from "../atoms/NavbarLogo";
 
 const NavbarElements = () => {
 
    return (
+      
       <menu>
+         <NavbarLogo/>
          <NavbarItem label={"Game"}/>
          <NavbarItem label={"Langs"}/>
          <NavbarItem label={"Pokédex"}/>
