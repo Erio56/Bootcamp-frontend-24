db70a47979db9fc758a44ede78a77f88424151ec
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-28 16:59:14 -0500

    redo of fetching logic

--- final-proyect/src/API/PokeApi.ts
+++ final-proyect/src/API/PokeApi.ts
@@ -1,12 +1,9 @@
 import { ISpecieResult } from "./models/ISpeciesReturn";
 
-const getPokemonList = () => {
-   return fetch('https://pokeapi.co/api/v2/pokemon-species?limit=100') as unknown as ISpecieResult;
-}
 
 export const getRandomPokemons = async () => {
-   const pokemons = await getPokemonList();
-   const randomPokemons = pokemons.results.sort(() => 0.5 - Math.random()).slice(0, 4);
+   const speciesResponse = await fetch('https://pokeapi.co/api/v2/pokemon-species?limit=100');
+   const species = await speciesResponse.json() as ISpecieResult;
 
-   return randomPokemons;
+   return species;
 }
