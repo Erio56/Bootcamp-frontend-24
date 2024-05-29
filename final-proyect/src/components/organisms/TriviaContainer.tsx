283ea9d23686697ed71325bf4640c724f3053795
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-29 14:11:33 -0500

    added game tries logic

--- final-proyect/src/components/organisms/TriviaContainer.tsx
+++ final-proyect/src/components/organisms/TriviaContainer.tsx
@@ -1,29 +1,29 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import usePokemon from "../../API/PokeApi";
 import PokemonImage from "../atoms/PokemonImage";
 import TriviaButton from "../atoms/TriviaButton";
 import { Pokemon } from "../../API/models/Pokemon";
-import { GeneralContext } from "../../contexts/GeneralContex";
 
 const TriviaContainer = () => {
-  
   const [triviaPokemon, setTriviaPokemon] = useState<Pokemon | null>(null);
   const [score, setScore] = useState(0);
   const [tries, setTries] = useState(0);
 
   const { pokemons, error } = usePokemon(tries);
-  
+
   useEffect(() => {
-    if (pokemons.length > 0) {
+    if (score <= 3) {
       setTriviaPokemon(pokemons[Math.floor(Math.random() * pokemons.length)]);
     }
-  }, [pokemons]);
+  }, [pokemons, score]);
 
   const handleClick = (pokemonId: number) => {
-    setTries(tries + 1);
+    if (tries <= 3) {
+      setTries(tries + 1);
 
-    if (pokemonId === triviaPokemon?.id) {
-      setScore(score + 1);
+      if (pokemonId === triviaPokemon?.id) {
+        setScore(score + 1);
+      }
     }
   };
 
@@ -33,16 +33,21 @@ const TriviaContainer = () => {
 
   return (
     <div>
-      {triviaPokemon && <PokemonImage pokemonId={triviaPokemon.id} />}
-      {pokemons.map((pokemon) => (
-        <TriviaButton 
-          key={pokemon.id} 
-          pokemon={pokemon} 
-          handleOnClick={() => handleClick(pokemon.id)} 
-        />
-      ))}
+      {tries === 3 && <div>Game over your score is {score}</div>}
+      {tries < 3 && (
+        <div>
+          {triviaPokemon && <PokemonImage pokemonId={triviaPokemon.id} />}
+          {pokemons.map((pokemon) => (
+            <TriviaButton
+              key={pokemon.id}
+              pokemon={pokemon}
+              handleOnClick={() => handleClick(pokemon.id)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default TriviaContainer;
