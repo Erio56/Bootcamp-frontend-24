b8854edb6d6182d11de4da411557f4a61a1cb099
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-29 07:33:35 -0500

    added hook and game logic

--- final-proyect/src/components/organisms/TriviaContainer.tsx
+++ final-proyect/src/components/organisms/TriviaContainer.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react";
+import usePokemon from "../../API/PokeApi";
 import PokemonImage from "../atoms/PokemonImage";
 import TriviaButton from "../atoms/TriviaButton";
+import { Pokemon } from "../../API/models/Pokemon";
 
 const TriviaContainer = () => {
 
+   const { pokemons, error } = usePokemon();
+   
+   const [triviaPokemon, setTriviaPokemon] = useState<Pokemon | null>(null)
+   const [score, setScore] = useState(0);
+   const [tries, setTries] = useState(0);
+
+   useEffect(() => {
+      setTriviaPokemon(pokemons[Math.floor(Math.random() * pokemons.length)]);
+   }, [pokemons])
+
+   const handleClick = (pokemonId: number) => {
+      setTries( tries + 1);
+
+      if(pokemonId === triviaPokemon?.id){
+         setScore(score + 1);
+      }
+   }
+   
+
    return (
       <div>
-         <PokemonImage/>
-         <TriviaButton/>
-         <TriviaButton/>
-         <TriviaButton/>
+         <p>{JSON.stringify(triviaPokemon)}</p>
+         <p>{score}</p>
+         <p>{tries}</p>
+         <PokemonImage pokemonId={triviaPokemon?.id}/>
+         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[0].id) }}/>
+         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[1].id) }}/>
+         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[2].id) }}/>
       </div>
    )
 }
