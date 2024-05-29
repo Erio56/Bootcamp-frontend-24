f1208db35a3743090f851cf424d4c0b55fbdc32e
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-29 07:41:29 -0500

    add pokemon hook rerun after trivia button click

--- final-proyect/src/components/organisms/TriviaContainer.tsx
+++ final-proyect/src/components/organisms/TriviaContainer.tsx
@@ -5,37 +5,49 @@ import TriviaButton from "../atoms/TriviaButton";
 import { Pokemon } from "../../API/models/Pokemon";
 
 const TriviaContainer = () => {
+  const [fetchTrigger, setFetchTrigger] = useState(0); 
 
-   const { pokemons, error } = usePokemon();
-   
-   const [triviaPokemon, setTriviaPokemon] = useState<Pokemon | null>(null)
-   const [score, setScore] = useState(0);
-   const [tries, setTries] = useState(0);
+  const { pokemons, error } = usePokemon(fetchTrigger);
+  
+  const [triviaPokemon, setTriviaPokemon] = useState<Pokemon | null>(null);
+  const [score, setScore] = useState(0);
+  const [tries, setTries] = useState(0);
 
-   useEffect(() => {
+  useEffect(() => {
+    if (pokemons.length > 0) {
       setTriviaPokemon(pokemons[Math.floor(Math.random() * pokemons.length)]);
-   }, [pokemons])
+    }
+  }, [pokemons]);
 
-   const handleClick = (pokemonId: number) => {
-      setTries( tries + 1);
+  const handleClick = (pokemonId: number) => {
+    setTries(tries + 1);
 
-      if(pokemonId === triviaPokemon?.id){
-         setScore(score + 1);
-      }
-   }
-   
+    if (pokemonId === triviaPokemon?.id) {
+      setScore(score + 1);
+    }
 
-   return (
-      <div>
-         <p>{JSON.stringify(triviaPokemon)}</p>
-         <p>{score}</p>
-         <p>{tries}</p>
-         <PokemonImage pokemonId={triviaPokemon?.id}/>
-         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[0].id) }}/>
-         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[1].id) }}/>
-         <TriviaButton buttonName="test" handleOnClick={() => { handleClick(pokemons[2].id) }}/>
-      </div>
-   )
+    setFetchTrigger(prev => prev + 1);
+  };
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <p>{JSON.stringify(triviaPokemon)}</p>
+      <p>Score: {score}</p>
+      <p>Tries: {tries}</p>
+      {triviaPokemon && <PokemonImage pokemonId={triviaPokemon.id} />}
+      {pokemons.map((pokemon) => (
+        <TriviaButton 
+          key={pokemon.id} 
+          buttonName={pokemon.name} 
+          handleOnClick={() => handleClick(pokemon.id)} 
+        />
+      ))}
+    </div>
+  );
 }
 
 export default TriviaContainer;
