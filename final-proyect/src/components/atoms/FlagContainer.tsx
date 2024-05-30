349bbdc4b5bcdc9f6f9741187e61576742708c7e
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-30 13:28:32 -0500

    added unknown flag if language has no flag

--- final-proyect/src/components/atoms/FlagContainer.tsx
+++ final-proyect/src/components/atoms/FlagContainer.tsx
@@ -2,24 +2,35 @@ import { useContext } from "react";
 import { GeneralContext } from "../../contexts/GeneralContex";
 
 interface FlagContainer {
-   languageName: string
-   flagImage: string
+  languageName: string;
+  flagImage: string;
 }
 
 const FlagContainer = (props: FlagContainer) => {
-   const { currentLanguage, setCurrentLanguage } = useContext(GeneralContext);
+  const { currentLanguage, setCurrentLanguage } = useContext(GeneralContext);
 
-   const handleClick = (language: string) => {
-      setCurrentLanguage(language);
-   }  
+  const handleClick = (language: string) => {
+    setCurrentLanguage(language);
+  };
 
-
-   return (
-      <div onClick={() => { handleClick(props.languageName) }}>
-         <img src={props.flagImage} alt="" width={150} />
-         <p>{props.languageName}</p>
-      </div>
-   )
-}
+  return (
+    <div
+      onClick={() => {
+        handleClick(props.languageName);
+      }}
+    >
+      <img
+        src={props.flagImage}
+        alt=""
+        width={150}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = "assets/flags/unknown.png";
+        }}
+      />
+      <p>{props.languageName}</p>
+    </div>
+  );
+};
 
 export default FlagContainer;
