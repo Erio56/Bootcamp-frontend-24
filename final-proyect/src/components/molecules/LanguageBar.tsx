41ed544ac2766a7217836402d2036e53dc6b9f51
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-30 13:21:45 -0500

    added flags

--- final-proyect/src/components/molecules/LanguageBar.tsx
+++ final-proyect/src/components/molecules/LanguageBar.tsx
@@ -3,21 +3,30 @@ import FlagContainer from "../atoms/FlagContainer";
 import LanguageSelector from "../atoms/LanguageSelector";
 
 const FlagsBar = () => {
-
-  const [selectedLanguages, setSelectedLanguages] = useState<string[]>(['en','ja-Hrkt'])
+  
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>([
+    "en",
+    "ja-Hrkt",
+  ]);
 
   const handleSelectLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    if(!selectedLanguages.find(l => l === e.target.value)){
-      setSelectedLanguages(selectedLanguages.concat(e.target.value))
+    if (!selectedLanguages.find((l) => l === e.target.value)) {
+      setSelectedLanguages(selectedLanguages.concat(e.target.value));
     }
-  }
+  };
 
   return (
     <div>
-      <LanguageSelector handleOnChange={handleSelectLanguage}/>
+      <LanguageSelector handleOnChange={handleSelectLanguage} />
 
       <div>
-        {selectedLanguages.map(selectedLanguage => <FlagContainer flagImage={"assets/flags/spain.svg"} languageName={selectedLanguage} key={selectedLanguage}/>)}
+        {selectedLanguages.map((selectedLanguage) => (
+          <FlagContainer
+            flagImage={`assets/flags/${selectedLanguage}.svg`}
+            languageName={selectedLanguage}
+            key={selectedLanguage}
+          />
+        ))}
       </div>
     </div>
   );
