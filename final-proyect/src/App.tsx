f2f7a292aa399335d578ef02599a28be378ce3b6
Brayan Cataño <brayansanchez67@hotmail.com>
2024-05-30 14:07:30 -0500

    added routes

--- final-proyect/src/App.tsx
+++ final-proyect/src/App.tsx
@@ -1,12 +1,25 @@
 import "./styles/App.css";
 import HomePage from "./components/pages/HomePage";
 import GeneralContextProvider from "./contexts/GeneralContextProvider";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <HomePage />
+  },
+]);
 
 function App() {
   return (
     <>
       <GeneralContextProvider>
-        <HomePage />
+        <RouterProvider router={router}/>
       </GeneralContextProvider>
     </>
   );
