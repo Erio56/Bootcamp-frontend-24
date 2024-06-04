5010812e04a66e06fc95aa4510cbf14d69071281
Brayan Cataño <brayansanchez67@hotmail.com>
2024-06-04 14:18:33 -0500

    fixed webpack crashing

---
+++ webpack-demo/webpack.common.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'Development' }),
+  ],
+};
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,15 +5,14 @@ module.exports = {
    mode: 'development',
    entry: './src/index.js',
    output: {
-      path: path. resolve( __dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'main.js',
    },
-   devServer: {
-      static: './dist',
-    },
-   plugins: [new HtmlWebpackPlugin({title: 'Development',})],
    devtool: 'inline-source-map',
    devServer: {
       static: './dist',
    },
-}; 
+   plugins: [
+      new HtmlWebpackPlugin({ title: 'Development' }),
+   ],
+};
